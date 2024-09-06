939a04426c6d0475b27566acd4b6f2ef69254cae
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-09-06 13:53:45 -0700

    exported from Skill Struck

---
+++ Looping_Through_a_Dictionary.py
@@ -0,0 +1,15 @@
+arby = int(input("How many people is Jane bringing: "))
+
+garby = int(input("How many people is Amanda bringing: "))
+ride = { 
+    "terry"" : 10,
+    "lerry" : 3,
+    "jerry" : 6,
+    "clerry" : 30,
+    "harry" : garby,
+}
+
+total = 0
+for x,y in group.items():
+    total += x*y
+print(total)
